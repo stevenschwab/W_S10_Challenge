be07ceda90b3fa24c0bb4af5ce58aee1efb2d4cc
Steve Schwab <schwabs@umich.edu>
2024-04-04 15:30:10 -0700

    add pizza to the baseQuery

--- frontend/state/pizzaApi.js
+++ frontend/state/pizzaApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const pizzaApi = createApi({
     reducerPath: 'pizzaApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/pizza' }),
     endpoints: builder => ({
         getOrders: builder.query({
             query: () => 'history'
