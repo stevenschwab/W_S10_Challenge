2aad535bb6bcecfb5870969ab2f72f2fa4eab533
Steve Schwab <schwabs@umich.edu>
2024-04-04 16:45:12 -0700

    declare the type of tags used for invalidation

--- frontend/state/pizzaApi.js
+++ frontend/state/pizzaApi.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const pizzaApi = createApi({
     reducerPath: 'pizzaApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:9009/api/pizza' }),
+    tagTypes: ['Orders'],
     endpoints: builder => ({
         getOrders: builder.query({
             query: () => 'history'
